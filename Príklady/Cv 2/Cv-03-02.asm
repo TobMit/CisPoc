a004f3c7fc252164be68f88381baf2802142b2e9
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-09 15:50:44 +0100

    Spravenie ulohy 2

---
+++ Príklady/Cv 2/Cv-03-02.asm
@@ -0,0 +1,69 @@
+start:
+	scall key
+	cmi d,13
+	je koniec
+	cmi d, 49
+	je jeden
+	cmi d, 50
+	je dva
+	cmi d, 51
+	je tri
+	cmi d, 52
+	je stiri
+	cmi d, 53
+	je pet
+	cmi d, 54
+	je sest
+	cmi d, 55
+	je sedem
+	cmi d, 56
+	je osem
+	cmi d, 57
+	je devet
+	cmi d, 48
+	je nula
+
+nula:
+	mvi A, 0x03
+	out 0,A
+	jmp start
+jeden:
+	mvi A, 0x9F
+	out 0,A
+	jmp start
+dva:
+	mvi A, 0x25
+	out 0,A
+	jmp start
+tri:
+	mvi A, 0x0D
+	out 0,A
+	jmp start
+stiri:
+	mvi A, 0x99
+	out 0,A
+	jmp start
+pet:
+	mvi A, 0x49
+	out 0,A
+	jmp start
+sest:
+	mvi A, 0x41
+	out 0,A
+	jmp start
+sedem:
+	mvi A, 0x1F
+	out 0,A
+	jmp start
+
+osem:
+	mvi A, 0x01
+	out 0,A
+	jmp start
+
+devet:
+	mvi A, 0x09
+	out 0,A
+	jmp start
+
+koniec:
