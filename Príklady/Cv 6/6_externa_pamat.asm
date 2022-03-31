30a5dd03309dc76a81c44f86daf0d16f31a9fc82
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-31 19:52:59 +0200

    Pridaní súborov z 6 cvika

---
+++ Príklady/Cv 6/6_externa_pamat.asm
@@ -0,0 +1,7 @@
+;Praca s externou pamatou
+;schema: 6_externa_pamat.schx
+
+	mvi	a,55	;udaj, ktory chceme ulozit do pamate
+	smi	100,a	;ulozenie udaja na adresu 100
+
+	lmi	b,100	;precitanie udaja z adresy 100
